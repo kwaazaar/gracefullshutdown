6b89e648acbba0fa24a0ea3e46eacb03940bd049
Robert te Kaat <robert.te.kaat@gmail.com>
2022-11-09 11:15:11 +0100

    Worker added

---
+++ GracefullShutdown/SlowShutdownWorker/BackgroundWorker.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SlowShutdownWorker
+{
+    internal class BackgroundWorker : IHostedService
+    {
+        private readonly TimeSpan _shutdownDuration;
+        private readonly ILogger<BackgroundWorker> _logger;
+
+        public BackgroundWorker(ILogger<BackgroundWorker> logger, TimeSpan shutdownDuration)
+        {
+            _shutdownDuration = shutdownDuration;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Started with shutdown duration configured as {shutdownDuration}", _shutdownDuration);
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Shutting down using shutdown duration of {shutdownDuration}", _shutdownDuration);
+            await Task.Delay(_shutdownDuration); // Ignore cancellationtoken, since we really need to wait _shutdownDuration
+            if (cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogWarning("Shutdown completed, but took longer than allowed by cancellationToken");
+            }
+            else
+            {
+                _logger.LogInformation("Shutdown completed, which was allowed by cancellationToken (not cancelled yet)");
+            }
+        }
+    }
+}
---
+++ GracefullShutdown/SlowShutdownWorker/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SlowShutdownWorker;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            TimeSpan shutdownDuration = (args.Length == 1) ? TimeSpan.FromSeconds(int.Parse(args[0])) : TimeSpan.FromSeconds(60);
+            
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                    {
+                        services.Configure<HostOptions>(o => o.ShutdownTimeout = shutdownDuration.Add(TimeSpan.FromSeconds(1))); // Allow one extra second of total shutdown time
+
+                        services.AddHostedService(sp =>
+                            new BackgroundWorker(sp.GetRequiredService<ILogger<BackgroundWorker>>(),
+                            shutdownDuration));
+                    }
+                )
+                .RunConsoleAsync();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("PROGRAM EXCEPTION: {0}", ex);
+            return -1;
+        }
+    }
+}
